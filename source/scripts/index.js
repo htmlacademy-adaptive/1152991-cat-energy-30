bed2fdc243356f32be3bdbe22812f20e62e010a0
Danil <muravtsev.danil@mail.ru>
2024-01-12 14:41:57 +0300

    добавляет декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,66 @@
 /* в этот файл добавляет скрипты*/
+
+// menu
+const navToggle = document.querySelector('.main-header__toggle');
+const navMain = document.querySelector('.main-nav');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('touchend', () => {
+  navToggle.classList.toggle('main-header__toggle--close');
+  navMain.classList.toggle('main-nav--opened');
+});
+
+// map
+const screenWidth = window.screen.width;
+
+function initMap() {
+
+  const centerSize = creatCenter(screenWidth);
+
+  const map = new ymaps.Map('map', {
+    center: centerSize,
+    zoom: 16
+  });
+
+  const icon = iconGeometry(screenWidth);
+
+  const placemark = new ymaps.Placemark([59.93861885543524, 30.322878167387177], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'images/map-pin.svg',
+    iconImageSize: icon[0],
+    iconImageOffset: icon[1],
+  });
+
+  map.controls.remove('geolocationControl');
+  map.controls.remove('searchControl');
+  map.controls.remove('trafficControl');
+  map.controls.remove('typeSelector');
+  map.controls.remove('fullscreenControl');
+  map.controls.remove('zoomControl');
+  map.controls.remove('rulerControl');
+
+  map.geoObjects.add(placemark);
+}
+
+ymaps.ready(initMap);
+
+function creatCenter(screen) {
+  if (screen < 768) {
+    return [59.93861885543524, 30.322878167387177];
+  } else if (screen < 1440) {
+    return [59.938625584756636, 30.322974726911703];
+  } else {
+    return [59.93833891445115, 30.319751092081667];
+  }
+}
+
+function iconGeometry(screen) {
+  if (screen < 768) {
+    return [[57, 53], [-27, -43]];
+  } else if (screen < 1439) {
+    return [[113, 106], [-57, -100]];
+  } else {
+    return [[113, 106], [-50, -95]];
+  }
+}
